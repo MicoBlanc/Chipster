abb50c96b5261ccb62b03ca3c9be62c727585370
Alvaro Gallego <alvaro@integratedprojects.co>
2024-11-07 09:00:42 -0500

    adding rollup to manage css bundle

---
+++ packages/chipster/rollup.config.mjs
@@ -0,0 +1,52 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import postcss from 'rollup-plugin-postcss'
+import terser from '@rollup/plugin-terser'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: true
+    }
+  ],
+  external: [
+    'react', 
+    'react-dom', 
+    'classnames',
+    'styled-components'
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+      exclude: [
+        '**/*.test.tsx',
+        '**/*.test.ts',
+        'src/__tests__/**/*',
+        'src/playwright/**/*'
+      ]
+    }),
+    postcss({
+      plugins: [
+        autoprefixer(),
+        cssnano()
+      ],
+      modules: {
+        generateScopedName: '[local]_[hash:base64:5]'
+      },
+      extract: 'styles.css',
+      minimize: true,
+      sourceMap: true
+    }),
+    terser()
+  ]
+} 
