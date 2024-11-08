ac9a73db36c91547063afd2f3a70b4a1318dc6b6
Alvaro Gallego <alvaro@integratedprojects.co>
2024-11-08 17:33:54 -0500

    ready for v1.2.4

--- packages/docs/components/demo/demoEmailShare.tsx
+++ packages/docs/components/demo/demoEmailShare.tsx
@@ -7,7 +7,7 @@ const codeSnippet = `import { Chipster } from '@micoblanc/chipster';
 
 export default function EmailShare() {
   return (
-    <Chipster>
+     <Chipster mode='free' joiner={[',', 'Tab', 'Enter']}>
       <Chipster.ItemList />
       <Chipster.Input 
         className="bg-white shadow-sm rounded-lg"
@@ -32,7 +32,7 @@ export default function DemoEmailShare() {
     <div className='font-sans flex flex-col items-center gap-2 justify-center w-full h-full'>
       <div className="px-3 py-6 w-full bg-neutral-50 max-w-xl border border-neutral-200 rounded-xl">
         <h2 className="text-base font-semibold mb-1 text-black">Share with your team</h2>
-        <Chipster mode='free' joiner={[';', 'Tab', 'Enter']}>
+        <Chipster mode='free' joiner={[',', 'Tab', 'Enter']}>
           <Chipster.ItemList />
           <Chipster.Input 
             className="bg-white shadow-sm rounded-lg"
